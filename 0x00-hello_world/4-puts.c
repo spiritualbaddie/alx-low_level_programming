1bbb61b059d5252b063f859719c9ccb4c17f8068
spiritualbaddie <wanjirumegan05@gmail.com>
2023-03-16 06:05:14 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -12,8 +12,7 @@
  * 2. The next line int main(void) is the main function where the program execution begins.
  *
  * 3. The next line puts("\"Programming is like building a multilingual puzzle");
- * causes the message Programming is like building a multilingual puzzle
- * to be displayed on the screen.
+ * causes the message to be displayed on the screen.
  *
  * 4.The next line return 0; terminates main( ) function and causes it to
  * return the value 0 to the calling process.

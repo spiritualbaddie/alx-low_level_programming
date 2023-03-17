8543eec296ad75b22f39435101ff9a345b8bb5ee
spiritualbaddie <wanjirumegan05@gmail.com>
2023-03-17 06:17:25 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,60 +1,32 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
-
+#include <ctype.h>
 /**
- * main - Prints the last digit of a randomly generated number
- * and whether it is greater than 5, less than 6, or 0.
+ * main - Entrypoint
  *
- * Return: Always 0.
+ * Return: Always 0 (success)
+ *
+ * FUNCTIONALITY *
+ *
+ * 1. The for loop is used to iterate through the alphabet.
+ *
+ * 2. The tolower() function is used to convert the letter to lowercase.
+ *
+ * 3. The putchar() function is used to print the letter.
+ *
+ * 4. The putchar() function is used to print a new line.
+ *
+ * Time Complexity: O(n)
  */
+
 int main(void)
 {
-	int n;
+	int letter;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-	if ((n % 10) > 5)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n",
-			n, n % 10);
-	}
-	else if ((n % 10) < 6 && (n % 10) != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-			n, n % 10);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is 0\n",
-			n, n % 10);
-	}
-
-	return (0);
-
-}
-
-
-2-print_alphabet.c
-
-#include <stdio.h>
-
-/**
- * main - Prints the alphabet.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
-
-	for (i = 0; i < 26; i++)
-	{
-		putchar(alp[i]);
+		letter = tolower(letter);
+		putchar(letter);
 	}
 	putchar('\n');
-	return (0);
-	}
-
+		return (0);
+}

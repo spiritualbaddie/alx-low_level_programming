6f80a73afa9bfcad3a4a466604414847a9e02609
spiritualbaddie <wanjirumegan05@gmail.com>
2023-03-17 06:53:39 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,46 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-/**
- * main - Prints the alphabet without q and e.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int i;
-
-	for (i = 97; i < 123; i++)
-	{
-		if (i != 101 && i != 113)
-		{
-			putchar(i);
-		}
-	}
-	putchar('\n');
-	return (0);
-}
-
-
-5-print_numbers.c
-
 #include <stdio.h>
-
 /**
- * main - Prints numbers from 0 to 9.
+ * main - Program to alphabet letters ecxept q and e followed by new line
  *
- * Return: Always 0 (Success)
+ * Return: return 0
+ *
+ * FUNCTIONALITY *
+ *
+ * 1. The for loop is initialized with letter = 'a' and the condition is letter <= 'z'.
+ *
+ * 2. The loop will continue until the condition is false.
+ *
+ * 3. The if statement checks if letter is equal to 'q' or 'e'.
+ *
+ * 4. If the condition is true, the continue statement is executed and the loop
+ * continues with the next iteration.
+ *
+ * 5. If the condition is false, the putchar function is executed and the letter is printed.
+ *
+ * 6. The loop continues until the condition is false.
+ *
+ * 7. The putchar function prints a new line.
+ *
+ * 8. The return statement returns 0.
+ *
+ * Time Complexity: O(n)
  */
 int main(void)
 {
-	int i;
+	int letter;
 
-	for (i = 0; i < 10; i++)
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		printf("%d", i);
+		if (letter == 'q' || letter == 'e')
+		{
+			continue;
+		}
+		putchar(letter);
 	}
 	putchar('\n');
-	return (0);
+return (0);
 }
-

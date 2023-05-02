5bdbeecfd7d86fe391d89ce3beac4c183bc54ce4
spiritualbaddie <wanjirumegan05@gmail.com>
2023-05-02 07:34:23 -0700

    code efficiency

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,22 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a linked list
- * @h: head of the list
- *
- * Return: the number of nodes
- */
+* print_listint - a func that prints all the elements of a list
+* @h: holds the head to the linked list
+* Return: the number of nodes
+**/
+
 size_t print_listint(const listint_t *h)
 {
-const listint_t *cursor = h;
-size_t count = 0;
+	size_t node_C;
+	const listint_t *next_ptr;
 
-while (cursor != NULL)
-{
-printf("%d\n", cursor->n);
-count += 1;
-cursor = cursor->next;
-}
-return (count);
-}
+	next_ptr = h;
+	node_C = 0;
 
+	while (next_ptr != NULL)
+	{
+		printf("%i\n", next_ptr->n);
+		node_C += 1;
+		next_ptr = next_ptr->next;
+	}
+
+	return (node_C);
+}

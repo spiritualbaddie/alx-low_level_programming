2614ed513c8145267071b4fefdb7d1e66dee663a
spiritualbaddie <wanjirumegan05@gmail.com>
2023-05-02 07:36:55 -0700

    fixing code

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,27 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
-*free_listint2 - frees a linked list
-*@head: pointer to the head of the list
+* free_listint2 - a func that frees a list
+* @head: a pointer to a pointer that point to a list
 *
-*Return: void
 */
 
 void free_listint2(listint_t **head)
 {
-listint_t *cursor;
-listint_t **temp = head;
+	listint_t *free_ptr;
+	listint_t *ptr;
 
-if (temp != NULL)
-{
-while (*head != NULL)
-{
-cursor = *head;
-free(cursor);
-*head = (*head)->next;
-}
-
-*temp = NULL;
-
-}
+	if (head != NULL)
+	{
+		ptr = *head;
+		while ((free_ptr = ptr) != NULL)
+		{
+			ptr = ptr->next;
+			free(free_ptr);
+		}
+		*head = NULL;
+	}
 }

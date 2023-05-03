e15d6d8b6dbac81f33b975c38dfa016060b5b733
spiritualbaddie <wanjirumegan05@gmail.com>
2023-05-03 02:16:02 -0700

    a rewrite

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,58 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - deletes a node at an index
- * @head: pointer to the head of the list
- * @index: index of the node to be added
- *
- * Return: the address of the node
- */
+* delete_nodeint_at_index - a function that deletes the node at
+*index index of a listint_t linked list.
+* @head: a pointer to a pointer point to head of list
+* @index: the index of the node that should be deleted
+* Return: 1 if it succeeded, -1 if it failed
+*/
+
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *old_node = NULL;
-	listint_t *previous_node = NULL;
-	unsigned int i = 0, list_len = listint_len(*head);
+	listint_t *previous_ptr;
+	listint_t *next_ptr;
+	unsigned int i;
 
-	if ((index > list_len) || (list_len == 0))
+	if (head == NULL || *head == NULL)
 		return (-1);
-	while (head != NULL)
-	{
-		if (i == index)
-		{
-			old_node = *head;
-			if (i == 0)
-			{
-				*head = old_node->next;
-				free(old_node);
-				return (1);
-			}
-			previous_node->next = old_node->next;
-			free(old_node);
-			return (1);
-		}
-		else if ((i + 1) == index)
-			previous_node = *head;
-		head = &((*head)->next);
-		i++;
-	}
-	return (-1);
-}
 
-/**
- * listint_len - counts the number of nodes in a linked list
- * @h: head of the list
- *
- * Return: the number of elements
- */
-size_t listint_len(const listint_t *h)
-{
-	const listint_t *cursor = h;
-	size_t count = 0;
-
-	while (cursor != NULL)
+	next_ptr = *head;
+	previous_ptr = NULL;
+	if (index == 0)
 	{
-		count += 1;
-		cursor = cursor->next;
+		*head = (*head)->next;
+		free(next_ptr);
+		return (1);
 	}
-	return (count);
+	for (i = 0; i < index; i += 1)
+	{
+		if (next_ptr == NULL)
+			return (-1);
+		previous_ptr = next_ptr;
+		next_ptr = next_ptr->next;
+	}
+	if (previous_ptr)
+		previous_ptr->next = next_ptr->next;
+	free(next_ptr);
+
+	return (1);
 }

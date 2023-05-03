d49a4b7afc244a9fc4e4f659a9c3f833b0aacf1c
spiritualbaddie <wanjirumegan05@gmail.com>
2023-05-03 02:14:16 -0700

    out with the old in with the new

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,64 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - returns the nth node of a linked list
- * @head: pointer to the head of the list
- * @idx: index of the node to be added
- * @n: content of the new node
- *
- * Return: the address of the node
- */
+* insert_nodeint_at_index - a function that inserts a new
+*node at a given position
+* @head: a pointer to a pointer to a struct
+* @idx: is the index of the list where the new node
+*should be added. Index starts at 0
+* @n: the data for the new node
+* Return: the address of the new node, or NULL if it failed
+*/
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node = NULL;
-	listint_t *previous_node = NULL;
-	unsigned int i = 0;
+	listint_t *next_ptr;
+	listint_t *previous_ptr;
+	listint_t *new_node;
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+	next_ptr = *head;
+	previous_ptr = NULL;
+	for (i = 0; i < idx; i += 1)
+	{
+		if (next_ptr == NULL)
+			return (NULL);
+		previous_ptr = next_ptr;
+		next_ptr = next_ptr->next;
+	}
 
 	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL || idx > listint_len(*head))
-	{
-		free(new_node);
+	if (!new_node)
 		return (NULL);
-	}
 	new_node->n = n;
-	new_node->next = NULL;
-	while (head != NULL)
-	{
-		if (i == idx)
-		{
-			if (i == 0)
-			{
-				new_node->next = *head;
-				*head = new_node;
-				return (new_node);
-			}
-			new_node->next = previous_node->next;
-			previous_node->next = new_node;
-			return (new_node);
-		}
-		else if ((i + 1) == idx)
-			previous_node = *head;
-		head = &((*head)->next);
-		i++;
-	}
-	return (NULL);
-}
+	new_node->next = next_ptr;
+	if (idx == 0)
+		*head = new_node;
+	else
+		previous_ptr->next = new_node;
 
-/**
- * listint_len - counts the number of nodes in a linked list
- * @h: head of the list
- *
- * Return: the number of elements
- */
-size_t listint_len(const listint_t *h)
-{
-	const listint_t *cursor = h;
-	size_t count = 0;
-
-	while (cursor != NULL)
-	{
-		count += 1;
-		cursor = cursor->next;
-	}
-	return (count);
+	return (new_node);
 }

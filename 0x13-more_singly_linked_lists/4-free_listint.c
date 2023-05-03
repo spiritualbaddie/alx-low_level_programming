77ec06fc30fc1c6dd0d51ba2df7d9daba177aa4d
spiritualbaddie <wanjirumegan05@gmail.com>
2023-05-03 02:08:32 -0700

    code change

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,20 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "lists.h"
 
 /**
-*free_listint - frees a linked list
-*@head: head of the list
-*
-*Return: void
+* free_listint - a func that frees a list
+* @head: a pointer point to the head of a list
 */
 
 void free_listint(listint_t *head)
 {
-listint_t *temp;
+	listint_t *free_ptr;
 
-while (head != NULL)
-{
-temp = head;
-head = head->next;
-free(temp);
-}
+	while (head != NULL)
+	{
+		free_ptr = head;
+		head = head->next;
+		free(free_ptr);
+	}
 }
